56e99925fc919251bb48820f6db30c8ad1f06c90
was <zxt_zel_npl@163.com>
2018-07-30 15:25:37 +0800

    add after stash

--- invariant-test/index.js
+++ invariant-test/index.js
@@ -1,2 +1,10 @@
 var invariant = require('invariant');
 
+var someTruthyVal=true;
+var someFalseyVal=false;
+
+invariant(someTruthyVal, 'This will not throw');
+// No errors
+
+invariant(someFalseyVal, 'This will throw an error with this message');
+// Error: Invariant Violation: This will throw an error with this message
